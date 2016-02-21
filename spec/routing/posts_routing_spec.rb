bd920fedd2d7729f6e38a4bb50b62f417cef384f
Wouter de Vos <wouter@codaisseur.com>
2016-02-21 21:54:13 +0100

    Routing for Posts

---
+++ spec/routing/posts_routing_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :routing do
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(:get => "/").to route_to("posts#index")
+    end
+
+    it "routes to #show" do
+      expect(:get => "/posts/1").to route_to("posts#show", id: "1")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/posts").to route_to("posts#create")
+    end
+  end
+end
+
